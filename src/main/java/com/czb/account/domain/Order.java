a3b59e127db349bff43a27e1f8b483cdceaa4599
zyj <zhyanjuncn@163.com>
2022-01-13 00:57:44 +0800

    'id'

--- src/main/java/com/czb/account/domain/Order.java
+++ src/main/java/com/czb/account/domain/Order.java
@@ -6,7 +6,10 @@ import lombok.Data;
 @Data
 public class Order extends Model<Order> {
 
+    //订单id
     private Long userId;
+    //skuId
     private Long skuId;
+    //spuId
     private Long spuId;
 }
