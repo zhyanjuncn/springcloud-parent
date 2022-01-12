0639b853b4594a3e30e1fa9b03a7576253600eb4
zyj <zhyanjuncn@163.com>
2022-01-12 14:33:13 +0800

    'code'

---
+++ src/main/java/com/czb/account/domain/User.java
@@ -0,0 +1,13 @@
+package com.czb.account.domain;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
+
+@Data
+public class User extends Model<User> {
+
+    private Long id;
+    private String nickname;
+    private String password;
+    private String provinceName;
+}
