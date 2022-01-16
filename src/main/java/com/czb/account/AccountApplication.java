9c2aee78904592babf74bed8700f2f334bea26f7
zyj <itzhangyanjun@163.com>
2022-01-16 23:43:48 +0800

    'code'

--- src/main/java/com/czb/account/AccountApplication.java
+++ src/main/java/com/czb/account/AccountApplication.java
@@ -1,11 +1,22 @@
 package com.czb.account;
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@MapperScan("com.czb.account.mapper")
 public class AccountApplication {
+    private final static Logger LOGGER = LoggerFactory.getLogger(AccountApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class,args);
     }
 
+    @PostConstruct
+    public void outLog(){
+        LOGGER.info("当前系统时间为：{}",System.currentTimeMillis());
+    }
 }
