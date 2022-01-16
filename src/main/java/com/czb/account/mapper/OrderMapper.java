cd3e429a4f6e543531eee7c39aed0d631a1c12b1
zyj <itzhangyanjun@163.com>
2022-01-17 00:07:28 +0800

    'msg'

---
+++ src/main/java/com/czb/account/mapper/OrderMapper.java
@@ -0,0 +1,8 @@
+package com.czb.account.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.czb.account.domain.Account;
+import com.czb.account.domain.Order;
+
+public interface OrderMapper extends BaseMapper<Order> {
+}
