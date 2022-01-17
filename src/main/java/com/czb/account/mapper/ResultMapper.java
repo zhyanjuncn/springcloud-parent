445eed411d97cbb509636b8fdd42159df0c5dcd5
zyj <itzhangyanjun@163.com>
2022-01-17 23:56:30 +0800

    'code'

---
+++ src/main/java/com/czb/account/mapper/ResultMapper.java
@@ -0,0 +1,9 @@
+package com.czb.account.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.czb.account.domain.Result;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ResultMapper extends BaseMapper<Result> {
+}
